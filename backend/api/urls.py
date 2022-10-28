265db3811cc17f30d154c3c48198bb8979c10ed2
grce47 <gracesharma47@gmail.com>
2022-10-29 00:59:25 +0530

    added routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.get_user_portfolio,name='api-user-portfolio'), #shows routes
+    path('',views.get_routes,name='get-routes'),
+    path('user',views.get_user_portfolio,name='api-user-portfolio'), #shows routes
+    path('order',views.add_new_order,name='new-order'),
+    path('price',views.get_market_price,name='market-price'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,104 @@
 from rest_framework.response import Response
-from myUser.models import Person
+from myUser.models import Person,Pending_Buy_Order,Pending_Sell_Order
 from .serializers import PersonSerializers
 from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        {
+            'url' : 'http://127.0.0.1:8000/order',
+            'desc' : 'GET : Requirements for Post method. POST : Add new order in market',
+            'method' : '{GET,POST}'
+        },
+        {
+            'url' : 'http://127.0.0.1:8000/user',
+            'desc' : 'User Portfolio',
+            'method' : 'GET'
+        },
+        {
+            'url' : 'http://127.0.0.1:8000/price',
+            'desc' : 'Sends Current Market Price as JSON file',
+            'method' : 'GET'
+        }
+    ]
+    return Response(routes)
+
 
 @api_view(['GET'])
 def get_user_portfolio(request):
     person = PersonSerializers(Person.objects.all(),many=True)
     return Response(person.data) #shows what queries can be made
+
+@api_view(['GET','POST'])
+def add_new_order(request):
+    if request.method == 'GET':
+        requirements = {
+            'buy_or_sell' : 'buy/sell',
+            'user' : 'A/B/C/...',
+            'order_type' : 'limit/market',
+            'stock_amount' : 'interger value',
+            'price': 'float value (not required for order_type = market)'
+        }
+        return Response(requirements)
+    elif request.method == 'POST':
+        buy_or_sell = request.data.get('buy_or_sell',None)
+        user = request.data.get('user',None)
+        order_type = request.data.get('order_type',None)
+        stock_amount = request.data.get('stock_amount',None)
+        price = request.data.get('price',None)
+
+        if buy_or_sell == None:
+            return Response({'error':'buy_sell info not found'})
+        if buy_or_sell != 'buy' and buy_or_sell != 'sell':
+            return Response({'error' : 'wrong choice in buy_or_sell'})
+
+        if user == None:
+            return Response({'error' : 'user info not found'})
+        if len(Person.objects.filter(name=user)) == 0:
+            return Response({"error" : "user not found in database"})
+
+        if order_type == None:
+            return Response({"error" : "order_type not found"})
+        if order_type != "limit" and order_type != "market":
+            return Response({"error" : "Wrong Choice for order_type"})
+        
+        if stock_amount == None:
+            return Response({"error" : "stock_amount not found"})
+        
+        try:
+            stock_amount = int(stock_amount)
+        except:
+            return Response({"error" : "Wrong type of stock amount"})
+        
+        if order_type == 'limit':
+            if price == None:
+                return Response({"error" : "price not found"})
+            try:
+                price = float(price)
+            except:
+                return Response({"error" : "Wrong type of price"})
+            # TODO : Apply limit rules
+
+
+
+
+
+
+            return Response({"success" : "limit order"})
+        else:
+            # TODO : Apply market rules
+
+
+
+
+            return Response({"success" : "market order"})
+
+
+
+MARKET_PRICE = 100
+@api_view(['GET'])
+def get_market_price(request):
+    global MARKET_PRICE
+    MARKET_PRICE += 1
+    return Response({'market_price' : MARKET_PRICE})
